f9a528ecfbc0b15fee8b30e2245172af7421cc3e
Michael O'Hanlon <hypezx@gmail.com>
2024-08-08 14:44:40 +0100

    added update tasks

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
@@ -29,12 +30,14 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/add", addHandler)
 	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/update", updateHandler) // Add this line
 	http.HandleFunc("/save", saveHandler)
 	http.HandleFunc("/load", readHandler)
 	http.HandleFunc("/deleteAll", deleteAllHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.ListenAndServe(":8080", nil)
+	fmt.Println("server started on port http://localhost:8080/")
 }
 
 func check(e error) {
@@ -78,12 +81,26 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		indexStr := r.FormValue("index")
+		newName := r.FormValue("newName")
+		index, err := strconv.Atoi(indexStr)
+		if err == nil && newName != "" {
+			tasks.Lock()
+			if index >= 0 && index < len(tasks.Tasks) {
+				tasks.Tasks[index].Name = newName
+			}
+			tasks.Unlock()
+		}
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
 func deleteAllHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		tasks.Lock()
-		if len(tasks.Tasks) >= 0 {
-			tasks.Tasks = nil
-		}
+		tasks.Tasks = nil
 		tasks.Unlock()
 	}
 	http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -111,7 +128,6 @@ func readHandler(w http.ResponseWriter, r *http.Request) {
 		tasks.Lock()
 		defer tasks.Unlock()
 
-		// Check if the tasks array is empty
 		if len(tasks.Tasks) == 0 {
 			dat, err := os.ReadFile("savedTasks.json")
 			check(err)
